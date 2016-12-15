884c13b41faf0acf469892d513270dde4bad9255
Reuben Pasquini <reuben@frickjack.com>
2016-12-14 22:27:24 -0600

    little more gulpfile setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp');
+var clean = require('gulp-rimraf');
+
+gulp.task('clean', [], function() {
+  console.log("Clean all files in build folder");
+
+  return gulp.src("build/*", { read: false }).pipe(clean());
+});
 
 gulp.task('default', function() {
   // place code for your default task here
   console.log( "Hello, World!" );
+  gulp.src( "web/**/*" ).pipe( gulp.dest( "build/" ) );
 });
