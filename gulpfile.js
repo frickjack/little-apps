e7de48a330a720aa2fbed50188c6c7f17a49c0c0
Reuben Pasquini <reuben@frickjack.com>
2016-12-30 22:50:03 -0600

    arrivalPie introduce nunjucks build-time templating
    wire up arrivalPie custom element to use html-template

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var clean = require('gulp-rimraf');
 var ts = require('gulp-typescript');
+var nunjucksRender = require('gulp-nunjucks-render');
 //var tsProject = ts.createProject("tsconfig.json");
 //var watch = require( 'gulp-watch' );
 
@@ -15,9 +16,20 @@ gulp.task( 'compilejs', [], function() {
     gulp.src( "web/**/*.js" ).pipe( gulp.dest( "build/" ) );
 });
 
-gulp.task( 'compilehtml', [], function() {
-    gulp.src( "web/**/*.html" ).pipe( gulp.dest( "build/" ) );
+// Server side templating with nunjucks
+gulp.task( 'compilenunjucks', [], function() {
+    gulp.src( ["web/**/*.html", "!web/blog/gridDemo.html", "!web/eventTrack/events.html" ] )
+    .pipe( nunjucksRender( [ "build/templates" ] ) )
+    .on('error', console.log)
+    .pipe( gulp.dest( "build/" ) );
 });
+
+gulp.task( 'compilehtml', [ 'compilenunjucks'], function() {
+    gulp.src( "web/blog/*.html" )
+    .pipe( gulp.dest( "build/blog/" ) );
+});
+
+
 
 gulp.task( 'compilecss', [], function() {
     gulp.src( "web/**/*.css" ).pipe( gulp.dest( "build/" ) );
@@ -51,4 +63,15 @@ gulp.task('default', [ 'compile' ], function() {
 gulp.task('watchts', function () {
     // Endless stream mode 
     return gulp.watch('web/**/*.ts', [ 'compilets' ] );
+});
+
+gulp.task( 'watchhtml', function () {
+   return gulp.watch( 'web/**/*.html', [ 'compilehtml' ] );     
+});
+
+gulp.task( 'watchcss', function () {
+   return gulp.watch( 'web/**/*.css', [ 'compilecss' ] );     
+});
+
+gulp.task( 'watch', [ 'watchts', 'watchhtml', 'watchcss' ], function() {
 });
--- web/resources/js/littleware/arrivalPie/arrivalPie.ts
+++ web/resources/js/littleware/arrivalPie/arrivalPie.ts
@@ -1,10 +1,4 @@
 module littleware.arrivalPie {
-    
-/**
- * ArrivalPie custom element
- */
-class ArrivalPie extends HTMLElement {
-}
 
 export interface CustomeElements {
     define( tagName:string, elementClass:any ):void;
@@ -12,6 +6,92 @@ export interface CustomeElements {
 
 declare var customElements: CustomeElements;
 
-customElements.define( "arrival-pie", ArrivalPie );
+customElements.define( "lw-arrival-pie", 
+        
+    /**
+     * ArrivalPie custom element
+     */
+    class ArrivalPie extends HTMLElement {
+        // Can define constructor arguments if you wish.
+        constructor() {
+          // If you define a ctor, always call super() first!
+          // This is specific to CE and required by the spec.
+          super();
+
+          /*
+          // Setup a click listener on <app-drawer> itself.
+          this.addEventListener('click', e => {
+            // Don't toggle the drawer if it's disabled.
+            if (this.disabled) {
+              return;
+            }
+            this.toggleDrawer();
+          });
+          */
+          let template = document.querySelector( 'template[id="lw-arrival-pie-top"]' ) as HTMLTemplateElement;
+          if ( ! template ) {
+            throw new Error( "ArrivalPie template not loaded: lw-arrival-pie-top" );
+          }
+          let clone = document.importNode( template.content, true );
+          //this.innerHTML = "<div><h3>Hello from ArrivalPie!</h3></div>";
+          this.appendChild( clone );
+        }
+
+        onnectedCallback(): void {
+        }
+
+        disconnectedCallback(): void {
+        }
+
+        attributeChangedCallback(attrName?: string, oldVal?: string, newVal?: string): void {
+        }
+
+        adoptedCallback(): void {
+
+        }
+
+        addPath(): void {
+          let namespace = this.querySelector( "svg" ).namespaceURI;
+          let path = document.createElementNS( namespace, "path" );
+          // ...
+        }
+
+        
+        // A getter/setter for an open property.
+        get open() {
+          return this.hasAttribute('open');
+        }
+
+        set open(val) {
+          // Reflect the value of the open property as an HTML attribute.
+          if (val) {
+            this.setAttribute('open', '');
+          } else {
+            this.removeAttribute('open');
+          }
+          this.toggleDrawer();
+        }
+
+        // A getter/setter for a disabled property.
+        get disabled() {
+          return this.hasAttribute('disabled');
+        }
+
+        set disabled(val) {
+          // Reflect the value of the disabled property as an HTML attribute.
+          if (val) {
+            this.setAttribute('disabled', '');
+          } else {
+            this.removeAttribute('disabled');
+          }
+        }
+
+        
+        toggleDrawer() {
+          //...
+        }
+    }
+);
+
 
 }
