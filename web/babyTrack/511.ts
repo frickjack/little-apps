8713bceb187c2ae2bf8d4f914bfe80fdbb84f47f
Reuben Pasquini <reuben@frickjack.com>
2017-02-26 22:42:12 -0600

    Start 511SpecRunner.html setup

--- web/babyTrack/511.ts
+++ web/babyTrack/511.ts
@@ -6,6 +6,43 @@ namespace littleware {
             endTime: Date;
         }
 
+        export interface Stats {
+            avePeriodSecs:number;
+            aveDurationSecs:number;
+            timeCoveredSecs:number;
+            numSamples:number;
+        }
+
+
+        /**
+         * Compute statistics over the given history of contractions -
+         * assumes contractions are sorted in time-ascending order.
+         */
+        function computeStats( history:Contraction[] ):Stats {
+            let count = history.length;
+            let result = {
+                avePeriodSecs: 0,
+                aveDurationSecs:0,
+                timeCoveredSecs:0,
+                numSamples:count
+            };
+            if ( count > 1 ) {
+                let copy:Contraction[] = [].concat( history );
+                result.avePeriodSecs = history.slice(1).map( 
+                        (it) => { return it.startTime.getTime() - copy.shift().startTime.getTime(); } 
+                    ).reduce( (acc,it) => { return acc+it; }, 0 ) / (1000 * (count-1));
+            }
+            if ( count > 0 ) {
+                result.aveDurationSecs = Math.round(
+                    history.map( 
+                        (it) => { return it.endTime.getTime() - it.startTime.getTime(); }
+                    ).reduce( (acc,it) => { return acc + it; } ) / (1000 * count)
+                );
+                result.timeCoveredSecs = Math.round( (history[0].startTime.getTime() - history[count-1].endTime.getTime())/1000);
+            }
+            return result;
+        }
+
         /**
          * Manager for the 511 view
          */
---
+++ web/babyTrack/spec/511Spec.ts
@@ -0,0 +1,15 @@
+/// <reference path="../511.ts" />
+/// <reference path="../../resources/js/littleware/test/util.ts" />
+
+namespace littleware {
+    namespace app511 {
+        import arrivalPie = littleware.arrivalPie;
+        import littleTest = littleware.test;  
+
+        describe( "The 511 app controller", function() {
+            it( "Exists", function() {
+                expect( true ).toBe( 1 === 1 );
+            });
+        });
+    }
+}
