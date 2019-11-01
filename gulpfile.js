bf34f3294d258f4b5434c49c41aba006c3dc909a
Reuben Pasquini <reuben@frickjack.com>
2019-11-01 06:02:24 -0500

    fix(gulp-build): svg2png icon refactor

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const merge = require('merge-stream');
 const exec = require('child_process').exec;
 const mkdirp = require('mkdirp');
 const replace = require('gulp-replace');
+const svg2png = require('gulp-svg2png');
 const gulpHelper = require('@littleware/little-elements/gulpHelper');
 const package = require('./package.json');
 const basePath = "src/@littleware/little-apps";
@@ -10,67 +11,39 @@ const basePath = "src/@littleware/little-apps";
 // TODO - automate version assignment
 gulpHelper.defineTasks(gulp, { basePath, data: { jsroot: `/modules/${package.version}` } });
 
-
-gulp.task('makeico', function (cb) {
+// make the icon folder
+const makeFolder = function (path) {
     return new Promise( function(resolve, reject) {
-        const path = "site/resources/img/appIcons"; 
-        mkdirp( path, function(err) {
-                if (err) {
-                    console.log( err );
-                    reject( err );
-                } else {
-                    resolve( path );
-                }
+            mkdirp( path, function(err) {
+                    if (err) {
+                        console.log( err );
+                        reject( err );
+                    } else {
+                        resolve( path );
+                    }
+            });
         });
-    }).then( (folderStr) => {
-        const svgList = ['511', 'jwt'];
-        const rezList = [ '57', '72', '114', '144', '152', '167', '180'];
-        const promiseList = rezList.reduce( 
-                (acc, rez) => svgList.reduce(
-                    (acc, svgName) => {
-                        acc.push(
-                            {
-                                svgPath: `${basePath}/site/resources/img/${svgName}.svg`,
-                                pngPath: `${folderStr}/oo${svgName}.${rez}x${rez}.png`,
-                                rez:rez
-                            }
-                        );
-                        return acc;
-                    }, acc
-                ), []
-            ).map(
-                (info) => {
-                    return new Promise( function(resolve,reject) {
-                        const commandStr = `inkscape ${info.svgPath} --export-png ${info.pngPath} -w${info.rez} -h${info.rez}`;
-                        console.log( "makeico running command: " + commandStr );
-                        exec( commandStr, 
-                            function (err, stdout, stderr) {
-                                console.log(stdout);
-                                console.log(stderr);
-                                if ( err ) {
-                                    reject( err );
-                                } else {
-                                    resolve( info.pngPath );
-                                }
-                            }
-                        );
-                    });
-                }
-            );
-        return Promise.all( promiseList );
-    }).then(
-        (pngList) => {
-            cb();
-        }
-    ).catch(
-        (err) => {
-            console.log( "PNG creation failed", err );
-        }
+};
+
+const icoFolderPath = "web/site/resources/img/appIcons";
+    
+gulp.task('makeIcoFolder', function(cb) {
+    makeFolder(icoFolderPath).then(() => {cb();});
+});
+
+gulp.task('makeIco', function() {
+    const rezList = ['57', '72', '114', '144', '152', '167', '180'];
+    return merge.apply(
+        this, 
+        rezList.map(
+            rez => gulp.src(`${basePath}/site/resources/img/*.svg`)
+                    .pipe(svg2png({width:rez, height:rez}))
+                    .pipe(gulp.dest(`${icoFolderPath}/${rez}x${rez}`))
+        )
     );
 });
 
-
-gulp.task('compile', gulp.series('little-compile', 'makeico', function(done) {
+gulp.task('compile', gulp.series('little-compile', 'makeIco', function(done) {
   // place code for your default task here
   //console.log( "Hello, World!" );
   //gulp.src( "src/**/*" ).pipe( gulp.dest( "build/" ) );
@@ -88,10 +61,9 @@ gulp.task('default', gulp.series('compile', function(done) {
 /**
  * Prepare /dist folder for deployment
  */
-gulp.task('stage', gulp.series('little-compileclean', function() {
+gulp.task('stage', gulp.series('little-clean', 'compile', function() {
     return merge(
-        gulp.src('site/**/*.*'
-            //).pipe(replace(`/modules/`, `/modules/${package.version}/`)
+        gulp.src('web/site/**/*.*'
             ).pipe(gulp.dest('dist/')),
         gulp.src('node_modules/@littleware/little-elements/web/**/*.*'
             // hack for now - replace /modules/ path in styleHelper and basicShell
@@ -104,7 +76,7 @@ gulp.task('stage', gulp.series('little-compileclean', function() {
             ),
         //gulp.src('node_modules/@littleware/little-elements/maps/**/*.*').pipe(gulp.dest(`dist/modules/${package.version}/@littleware/little-elements/maps/`)),
         gulp.src('web/**/*.*').pipe(gulp.dest(`dist/modules/${package.version}/@littleware/little-apps/web/`)),
-        gulp.src('node_modules/lit-html/lit-html.js').pipe(gulp.dest(`dist/modules/${package.version}/lit-html/`)),
+        gulp.src('node_modules/lit-html/**/*.*').pipe(gulp.dest(`dist/modules/${package.version}/lit-html/`)),
         gulp.src('node_modules/font-awesome/**/*.*').pipe(gulp.dest(`dist/modules/${package.version}/font-awesome/`)),
         gulp.src('node_modules/jasmine-core/lib/jasmine-core/**/*.*').pipe(gulp.dest(`dist/modules/${package.version}/jasmine-core/lib/jasmine-core/`))
     );
